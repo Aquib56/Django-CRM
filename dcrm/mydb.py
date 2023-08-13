ebf6798dfdeaa9b417c96fab36d68c8f659b676a
Aquib <aquibs732@gmail.com>
2023-08-13 14:02:45 +0530

    Pushing all the files

---
+++ dcrm/mydb.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='erosennin',
+)
+cursorObject=dataBase.cursor()
+cursorObject.execute('CREATE DATABASE elderco')
+print("All Done!")
